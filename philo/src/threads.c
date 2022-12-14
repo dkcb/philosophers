f4f289ec8ea9b904caf4a5a41f4c1fda3422a0b1
dkcb <yes@exa.com>
2022-12-14 20:57:08 +0100

    fix all

--- philo/src/threads.c
+++ philo/src/threads.c
@@ -6,24 +6,54 @@
 /*   By: dkocob <dkocob@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/12/13 17:34:31 by dkocob        #+#    #+#                 */
-/*   Updated: 2022/12/13 23:42:47 by dkocob        ########   odam.nl         */
+/*   Updated: 2022/12/14 20:57:08 by dkocob        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philosophers.h"
 
+
+void *death_thread(void *val)
+{
+    struct s_data *data = (struct s_data *)val;
+    int i;
+
+    while (!death_check(data))
+    {
+        i = 0;
+        while (i < data->number_of_philosophers - 1)
+        {
+            if (data->philo_arr[i].time_to_live < time_current_long())
+            {
+                data->dead++;
+                pthread_mutex_lock(&data->mprint);
+                if (data->dead == 1)
+                    printf ("%5ld %d died\n", time_current_long() - data->time_start_long, i + 1);
+                pthread_mutex_unlock(&data->mprint);
+            }
+            i++;
+        }
+        pthread_mutex_unlock(&data->mdeath);
+        usleep(50);
+    }
+    return (NULL);
+}
+
 void *ft_phil_routine(void *val)
 {
     struct s_philosopher *philo = (struct s_philosopher *)val;
 
-    while (philo->eat_count != 0)
+    while (philo->eat_count != 0 && !death_check(philo->data))
     {
-        if (!ft_eat(philo) || death_set(philo) == 1)
-            return (NULL);
+        ft_eat(philo);
         philo->eat_count--;
-        if (!ft_sleep(philo) || death_set(philo) == 1)
+        if (death_check(philo->data))
             return (NULL);
-        if (!ft_think(philo) || death_set(philo) == 1)
+        ft_sleep(philo);
+        if (death_check(philo->data))
+            return (NULL);
+        ft_think(philo);
+        if (death_check(philo->data))
             return (NULL);
     }
     return (NULL);
@@ -36,13 +66,17 @@ int init_threads(struct s_data *data)
     i = 0;
     while (i < data->number_of_philosophers)
     {
-        usleep (i % 2 * 5);
         if (pthread_create(&data->threads[i], NULL, &ft_phil_routine, &data->philo_arr[i]) != 0)
         {
             perror("thread creation fails!\n");
             return (1);
         }
         i++;
+    }
+    if (pthread_create(&data->threads[201], NULL, &death_thread, data) != 0)
+    {
+        perror("thread creation fails!\n");
+        return (1);
     }
     return (0);
 }
